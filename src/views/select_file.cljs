b791e1007f881e38cf43175e69e3b7709b4bc2a9
Mylab <labs@onmycrowd.com>
2020-08-24 19:16:05 -0700

    Working in select-file component

--- src/views/select_file.cljs
+++ src/views/select_file.cljs
@@ -1,2 +1,12 @@
-(ns views.select-file)
+(ns views.select-file
+  (:require [re-frame.core :as re-frame]))
 
+(defn select-file-component []
+  [:span.upload-label
+   [:label
+    [:input.hidden-xs-up
+     {:type "file" :accept ".xml" :on-change put-upload}]
+    [:i.fa.fa-upload.fa-lg]
+    (or file-name "Seleccione el/los CFDIs")]
+   (when file-name
+     [:i.fa.fa-times {:on-click #(reset! app-state {})}])])
