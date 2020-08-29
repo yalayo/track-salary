18c7604066e127ae2c7c048adbf5cea29d5a5550
Mylab <labs@onmycrowd.com>
2020-08-28 22:02:16 -0700

    Working in processing the file. Adding re-frame code.

--- src/webapp/core.cljs
+++ src/webapp/core.cljs
@@ -3,14 +3,19 @@
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]
-   [cljs.core.async :refer [put! chan <! >!]])
+   [re-frame.core :as re-frame]
+   [cljs.core.async :refer [put! chan <! >!]]
+   [tubax.core :refer [xml->clj]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Let's do this!"}))
 
 ;; temp file uploading code
 ;; will be moved later to other files
 
 ;; atom to store file contents
-(def file-data (atom " "))
+;;(def file-data (atom ""))
 
 ;; transducer to stick on a core-async channel to manipulate all the weird javascript
 ;; event objects --- basically just takes the array of file objects or something
@@ -34,6 +39,10 @@
 (defn put-upload [e]
   (put! upload-reqs e))
 
+(defn process-file [e]
+  (put-upload e)
+  (re-frame/dispatch [:process-file])) ;; domino 1, dispatching the event
+
 ;; sit around in a loop waiting for a file to appear in the upload-reqs channel, read any such file, and when the read is successful, stick the file on the file-reads channel.
 (go-loop []
   (let [reader (js/FileReader.)
@@ -44,27 +53,43 @@
 
 ;; sit around in a loop waiting for a string to appear in the file-reads channel and put it in the state atom to be read by reagent and rendered on the page.
 (go-loop []
-  (reset! file-data (<! file-reads))
+  (reset! app-state (<! file-reads))
   (recur))
+
+(defn show-data [data]
+  (if (clojure.string/blank? data) "" (xml->clj data)))
+
+;; reframe
+;; domino 2, registering event handling
+(re-frame/reg-event-fx
+ :process-file
+ (fn [{:keys [db]} _]
+   {:db (assoc db :payment (show-data @app-state))}))
+
+;; domino 4, registering subscription
+(re-frame/reg-sub
+ :payment
+ (fn [db _]
+   (db :payment)))
+
 
 ;; input component to allow users to upload file.
 (defn input-component []
-  [:input {:type "file" :id "file" :accept ".xml" :name "file" :on-change put-upload}])
+  [:input {:type "file" :id "file" :accept ".xml" :name "file" :on-change process-file}])
 
 ;; ------------------------- 
 ;; Views
 
 (defn home-page []
+  (let [data (re-frame/subscribe [:payment])] ;; domino 5, subscribe
   [:div
    [:h2 "Welcome to Reagent"]
    [input-component]
-   [:p @file-data] ; render the file contents.
-   ])
+   [:p @data]
+   ]))
 
 ;; end 
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Let's do this!"}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
@@ -92,3 +117,4 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
   )
+
